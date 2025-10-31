7c552bc00eb72beda073cafb43780b3c1a71338e
Abhishek <appawar1857@gmail.com>
2025-10-31 16:30:45 +0530

    dropdownlist-fetched-from-application-properties

--- 07-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/example/thymeleafdemo/controller/StudentController.java
+++ 07-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/example/thymeleafdemo/controller/StudentController.java
@@ -1,15 +1,20 @@
 package com.example.thymeleafdemo.controller;
 
 import com.example.thymeleafdemo.model.Student;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 public class StudentController {
 
+    @Value("${countries}")
+    List<String>Country;
     @GetMapping("/showStudentForm")
     public String shwoFrom(Model theModel){
         //create a student object
@@ -17,6 +22,8 @@ public class StudentController {
 
         //add student object to the model
         theModel.addAttribute("student",theStudent);
+        //add countries list to the model from application.properties
+        theModel.addAttribute("countries",Country);
         return "student-form" ;
     }
 
