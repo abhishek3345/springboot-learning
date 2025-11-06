34bef1695948e8584186402f2d6aa537075da76d
Abhishek <appawar1857@gmail.com>
2025-11-06 16:31:46 +0530

    added regular expression and custom error message

--- 07-spring-boot-spring-mvc/validationdemo/src/main/java/com/example/Customer.java
+++ 07-spring-boot-spring-mvc/validationdemo/src/main/java/com/example/Customer.java
@@ -1,9 +1,6 @@
 package com.example;
 
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 public class Customer {
     private String firstName;
@@ -12,9 +9,13 @@ public class Customer {
     @Size(min=1, message = "is required")
     private String lastName;
 
+    @NotNull(message = "is required")
     @Min(value = 0, message = "must be >= 0")
     @Max(value = 10, message = "must be <= 10")
-    private int freePasses;
+    private Integer freePasses;
+
+    @Pattern(regexp = "^[0-9]{6}",message = "PIN Code must be 6 digits")
+    private String pinCode;
 
     public String getFirstName() {
         return firstName;
@@ -32,11 +33,19 @@ public class Customer {
         this.lastName = lastName;
     }
 
-    public int getFreePasses() {
+    public Integer getFreePasses() {
         return freePasses;
     }
 
-    public void setFreePasses(int freePasses) {
+    public void setFreePasses(Integer freePasses) {
         this.freePasses = freePasses;
+    }
+
+    public String getPinCode() {
+        return pinCode;
+    }
+
+    public void setPinCode(String pinCode) {
+        this.pinCode = pinCode;
     }
 }
