6103fdceca0fc60ce68a3d863e3a6e5aaec9b185
Abhishek <appawar1857@gmail.com>
2026-02-26 18:22:09 +0530

    userimpl

--- self-learning/demo/src/main/java/com/learning/demo/model/User.java
+++ self-learning/demo/src/main/java/com/learning/demo/model/User.java
@@ -4,17 +4,23 @@ import jakarta.persistence.*;
 import jdk.jfr.Enabled;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.management.ConstructorParameters;
+import java.util.Collection;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Entity
 @Getter
 @Setter
 
 @Table(name="user")
-public class User {
+public class User implements UserDetails {
     @Id
     @Column(name = "userid")
     private int userid;
@@ -50,5 +56,47 @@ public class User {
                 ", roles=" + roles +
                 '}';
     }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.roles.stream()
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+    }
+    @Override
+    public String getUsername(){
+        return username;
+    }
+
+    @Override
+    public String getPassword(){
+        return password;
+    }
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+    //  public Collection<? extends GrantedAuthority> getAuthorities(){
+//        return this.roles.stream()
+//                .map(role -> new SimpleGrantedAuthority(role))
+//                .collect(Collectors.toList());
+//  }
+
+
 }
 
--- self-learning/demo/src/main/java/com/learning/demo/service/UserDetailsServiceImpl.java
+++ self-learning/demo/src/main/java/com/learning/demo/service/UserDetailsServiceImpl.java
@@ -1,15 +1,36 @@
 package com.learning.demo.service;
 
+import com.learning.demo.model.User;
+import com.learning.demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.autoconfigure.WebMvcProperties;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
 
+    @Autowired
+    private UserRepository userRepository;
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+
+
+        Optional<User>userOptional = userRepository.findByUsername(username);
+        if(!userOptional.isPresent()){
+            throw new UsernameNotFoundException("User not found");
+        }
+        User user = userOptional.get();
+
+//        return org.springframework.security.core.userdetails.User.builder()
+//                .username(user.getUsername())
+//                .password(user.getPassword())
+//                .authorities(user.getAuthorities())
+//                .build();
+        return user;
     }
 }
